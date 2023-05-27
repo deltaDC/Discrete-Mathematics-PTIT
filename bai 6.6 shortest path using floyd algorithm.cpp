35a05b30c88b7f1f03b3b6123c319684e1ed3960
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2023-05-27 22:54:06 +0700

    Update bai 6.6 shortest path using floyd algorithm.cpp

--- bai 6.6 shortest path using floyd algorithm.cpp
+++ bai 6.6 shortest path using floyd algorithm.cpp
@@ -29,23 +29,23 @@ int main() {
 	}
     }
     ktao();
-	for(int k = 1; k <= n; k++)
-	{
-		for(int i = 1; i <= n; i++)
-		{
-			for(int j = 1; j <= n; j++)
-			{
-				d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
-			}
-		}
-	}
+    for(int k = 1; k <= n; k++)
+    {
 	for(int i = 1; i <= n; i++)
-        {
+	{
 		for(int j = 1; j <= n; j++)
 		{
-			cout << d[i][j] << " ";
+			d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
 		}
-		cout << endl;
 	}
+    }
+    for(int i = 1; i <= n; i++)
+    {
+	for(int j = 1; j <= n; j++)
+	{
+		cout << d[i][j] << " ";
+	}
+	cout << endl;
+    }
 }
 
