695f43bdff5b4912793516928da3cfcb604e5be7
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2023-06-04 01:27:51 +0700

    Update bai 6.6 shortest path using floyd algorithm.cpp

--- bai 6.6 shortest path using floyd algorithm.cpp
+++ bai 6.6 shortest path using floyd algorithm.cpp
@@ -1,51 +1,70 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-const int INF = 1e9;
+#define INF 99999
 
-int n, m; 
-int d[200][200];
-int a[200][100];
+void floydWarshall(vector<vector<int>>& graph, int V) {
+    vector<vector<int>> dist(V, vector<int>(V, INF));
+    vector<vector<int>> next(V, vector<int>(V, -1));
 
-void ktao()
-{
-	for(int i = 1; i <= n; i++)
-	{
-		for(int j = 1; j <= n; j++)
-		{
-			d[i][j] = a[i][j];
-		}
-	}
+    
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+            if (graph[i][j] != INF && i != j) {
+                next[i][j] = j;
+            }
+        }
+    }
+
+    
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                    next[i][j] = next[i][k];
+                }
+            }
+        }
+    }
+
+    
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cout << "K/c " << i + 1 << " -> " << j + 1 << " = " << dist[i][j] << ";  ";
+            if (dist[i][j] != INF && i != j) {
+                cout << i + 1;
+                int current = i;
+                while (current != j) {
+                    current = next[current][j];
+                    cout << " --> " << current + 1;
+                }
+            }else if(i==j) cout << i + 1;
+            cout << endl;
+        }
+        cout << endl;
+    }
 }
 
 int main() {
-    cin >> n;
-    for(int i = 1; i <= n; i++)
-    {
-    	for(int j = 1; j <= n; j++)
-    	{
-    		cin >> a[i][j];
-	}
-    }
-    ktao();
-    for(int k = 1; k <= n; k++)
-    {
-	for(int i = 1; i <= n; i++)
-	{
-		for(int j = 1; j <= n; j++)
-		{
-			d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
-		}
-	}
-    }
-    for(int i = 1; i <= n; i++)
-    {
-	for(int j = 1; j <= n; j++)
-	{
-		cout << d[i][j] << " ";
-	}
-	cout << endl;
-    }
-}
+    int V;
+    cin >> V;
 
+    vector<vector<int>> graph(V, vector<int>(V));
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cin >> graph[i][j];
+            if (graph[i][j] == 0 && i != j) {
+                graph[i][j] = INF;
+            }
+        }
+    }
+
+    floydWarshall(graph, V);
+
+    return 0;
+}
